fab003a1d8a9538d79134175411ad85ba2f4708e
samia.chowdhury <samia.chowdhury@infolytx.com>
2020-07-29 13:45:20 +0600

    add start and end notation in regex for request_dto validation

--- app/dto/request/hcc_request_dto.py
+++ app/dto/request/hcc_request_dto.py
@@ -7,7 +7,7 @@ from app.dto.base_dto import BaseDto
 class HCCRequestDto(BaseDto):
     icd_codes_list: List
     age: Optional[float] = 70
-    eligibility: Optional[str] = Field("CNA", regex="(CPA|CPD|CNA|CND|INS|CFA|CFD|NE|SNPNE)")
+    eligibility: Optional[str] = Field("CNA", regex="^(CPA|CPD|CNA|CND|INS|CFA|CFD|NE|SNPNE)$")
     medicaid: Optional[bool] = False
     original_reason_for_entitlement: Optional[str] = Field("0", regex="(0|1|2|3)")
-    sex: Optional[str] = Field("M", regex="(M|F)")
+    sex: Optional[str] = Field("M", regex="^(M|F)$")
