c8ab2ea256845ec444565324732913a993fd33ef
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2021-09-09 16:11:26 +0600

    refactor service to return only icd10 codes

--- app/service/impl/icd10_annotator_service_impl.py
+++ app/service/impl/icd10_annotator_service_impl.py
@@ -1,19 +1,18 @@
 from app.dto.response.icd10_annotation_response import ICD10AnnotationResponse
 from app.service.icd10_annotator_service import ICD10AnnotatorService
 from app.service.pipeline.components.dummy_component_one import DummyComponentOne
-from app.service.pipeline.components.dummy_component_three import DummyComponentThree
+from app.service.pipeline.components.icd10_annotation_dummy_component import ICD10AnnotationDummyComponent
 from app.service.pipeline.components.dummy_component_two import DummyComponentTwo
 from app.service.pipeline.pipeline_manager import PipelineManager
 
 
 class ICD10AnnotatorServiceImpl(ICD10AnnotatorService):
     def __init__(self):
-        self.__pipeline_components = [DummyComponentOne(), DummyComponentTwo(), DummyComponentThree()]
+        self.__pipeline_components = [DummyComponentOne(), DummyComponentTwo(), ICD10AnnotationDummyComponent()]
         self.__pipeline_manager = PipelineManager(self.__pipeline_components)
 
     def annotate_icd_10(self, text: str) -> ICD10AnnotationResponse:
         pipeline_result = self.__pipeline_manager.run_pipeline(text=text)
         return ICD10AnnotationResponse(
-            icd10_annotations=[pipeline_result[DummyComponentOne], pipeline_result[DummyComponentTwo],
-                               pipeline_result[DummyComponentThree]]
+            icd10_annotations=pipeline_result[ICD10AnnotationDummyComponent]
         )
