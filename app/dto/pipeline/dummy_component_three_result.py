e9dc5ec925afdcfc0acb35c000279c75cb756d4d
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2021-09-09 16:11:04 +0600

    add dummy icd10 component

--- app/dto/pipeline/dummy_component_three_result.py
+++
@@ -1,6 +0,0 @@
-from app.dto.pipeline.base_pipeline_component_result import BasePipelineComponentResult
-
-
-class DummyComponentThreeResult(BasePipelineComponentResult):
-    def __init__(self, message: str):
-        self.message = message
