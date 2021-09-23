4ad9f1c06e01b8d94397d244aaaf8cd7a0521f1e
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2021-09-23 15:31:13 +0600

    move dummy components to test folder

--- tests/service/pipeline/components/test_dummy_component_one.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from app.service.pipeline.components.dummy_component_one import DummyComponentOne
-
-
-class TestDummyComponentOne(TestCase):
-    def test__run__should_return_correct_response__given_correct_input(self):
-        result = DummyComponentOne().run({"text": "dummy"})
-        assert result[0].message == "dummy"
