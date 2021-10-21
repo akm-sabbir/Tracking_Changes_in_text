ba3fbe489237e99074580ba168057ac6858f8196
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2021-10-21 10:46:02 +0600

    update pipeline service params

--- app/service/icd10_pipeline_service.py
+++ app/service/icd10_pipeline_service.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
 
+from app.dto.core.icd10_pipeline_params import ICD10PipelineParams
 from app.dto.pipeline.base_pipeline_component_result import BasePipelineComponentResult
 
 
 class ICD10PipelineService(ABC):
 
     @abstractmethod
-    async def run_icd10_pipeline(self, note_id: str, text: str) -> BasePipelineComponentResult:
+    async def run_icd10_pipeline(self, params: ICD10PipelineParams) -> BasePipelineComponentResult:
         raise NotImplementedError()
