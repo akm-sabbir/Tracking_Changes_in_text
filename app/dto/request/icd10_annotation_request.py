c553ec6029b3d0229c5f8cea97d35c908ff4a851
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2021-11-02 10:49:36 +0600

    update validation message of icd10 request

--- app/dto/request/icd10_annotation_request.py
+++ app/dto/request/icd10_annotation_request.py
@@ -12,11 +12,11 @@ class ICD10AnnotationRequest(BaseDto):
     @validator('id')
     def id_must_be_string_and_nonempty(cls, id_value: str):
         if len(id_value.strip()) == 0 or re.match(r"true|false", id_value.lower()):
-            raise ValueError("must be string and cannot be empty")
+            raise ValueError("id must be string and cannot be empty")
         return id_value
 
     @validator('text')
     def text_must_be_string_and_nonempty(cls, text: str):
         if len(text.strip()) == 0 or re.match(r"[0-9.]+|true|false", text.lower()):
-            raise ValueError("must be string and cannot be empty")
+            raise ValueError("text must be string and cannot be empty")
         return text
