367f9238555c22afdc13c3c079c9b3682df739b3
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2021-11-02 10:49:17 +0600

    add caching to dynamodb get function

--- app/service/impl/dynamo_db_service.py
+++ app/service/impl/dynamo_db_service.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from decimal import Decimal
+from functools import lru_cache
 from typing import Any
 
 import boto3
@@ -17,12 +18,15 @@ class DynamoDbService(DBService):
         if self.dynamodb is None:
             self.dynamodb = boto3.resource('dynamodb')
         self.table = self.dynamodb.Table(table_name)
+        self.cache = {}
 
+    @lru_cache(maxsize=1024)
     def get_item(self, item_id: Any):
         try:
             response = self.table.query(
                 KeyConditionExpression=Key('id').eq(item_id)
             )
+            self.cache[item_id] = response['Items']
             return response['Items']
         except Exception:
             self.__logger.error("Error getting data from DynamoDB. Item Id: " + item_id, exc_info=True)
