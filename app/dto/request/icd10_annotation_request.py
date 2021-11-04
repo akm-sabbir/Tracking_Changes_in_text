868ef0f33c107578f07182557d122c0c31edfbd3
Sabbir, AKM <akm.sabbir@infolytx.com>
2021-11-04 11:12:15 +0000

    icd10_annotation_request.py edited online with Bitbucket

--- app/dto/request/icd10_annotation_request.py
+++ app/dto/request/icd10_annotation_request.py
@@ -15,8 +15,9 @@ class ICD10AnnotationRequest(BaseDto):
             raise ValueError("id must be string and cannot be empty")
         return id_value
 
-    @validator('text')
+   @validator('text')
     def text_must_be_string_and_nonempty(cls, text: str):
-        if len(text.strip()) == 0 or re.match(r"[0-9.]+|true|false", text.lower()):
+        if len(text.strip()) == 0 or re.match(r".*[a-zA-Z]+", text.lower()) is None:
+            print(text)
             raise ValueError("text must be string and cannot be empty")
         return text
