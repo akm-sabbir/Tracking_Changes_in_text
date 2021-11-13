5f03ec34ff8efcd1fbb2e06257a14455e5316da5
akm.sabbir <sabbirsourov@gmail.com>
2021-11-14 00:12:29 +0600

    commit annotation request dto

--- app/dto/request/icd10_annotation_request.py
+++ app/dto/request/icd10_annotation_request.py
@@ -10,14 +10,14 @@ class ICD10AnnotationRequest(BaseDto):
     text: str
 
     @validator('id')
-    def id_must_be_string_and_nonempty(cls, id_value: str):
+    def id_must_be_string_and_nonempty(cls, id_value: str) -> str:
         if len(id_value.strip()) == 0 or re.match(r"true|false", id_value.lower()):
             raise ValueError("id must be string and cannot be empty")
         return id_value
 
     @validator('text')
     def text_must_be_string_and_nonempty(cls, text: str):
-        if len(text.strip()) == 0 or re.match(r".*[a-zA-Z]+", text.lower()) is None:
-            print(text)
+        if len(text.strip()) == 0 or re.match(r".*[a-zA-Z]+", text.lower()) is None or \
+                re.match(r"true|false", text.lower()):
             raise ValueError("text must be string and cannot be empty")
         return text
