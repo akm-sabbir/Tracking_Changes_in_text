45f4a9c2b003076a01a77a1dd830e223a3347507
akm.sabbir <sabbirsourov@gmail.com>
2021-11-15 23:41:15 +0600

    added all the unit test removed some error in code

--- app/service/impl/icd10_exclusion_service_impl.py
+++ app/service/impl/icd10_exclusion_service_impl.py
@@ -8,7 +8,7 @@ from app.Settings import Settings
 class Icd10ExclusionServiceImpl(ICD10ExclusionService):
     icd_exclusion_util: ICDExclusions = ICDExclusions()
 
-    def __init__(self,):
+    def __init__(self, ):
         pass
 
     def get_icd_10_exclusion_service_(self, icd10_metainfo: dict) -> dict:
@@ -57,27 +57,24 @@ class Icd10ExclusionServiceImpl(ICD10ExclusionService):
             return exclusion_list
 
     def get_not_selected_icd10_list(self, key: str, exclusion_list: list, icd10_metainfo: dict) -> list:
-        resultant_icd10s = []
         if icd10_metainfo.get(key).hcc_map is None and \
                 self.get_exclusion_list_hccmap(exclusion_list, icd10_metainfo) is False:
             return self.get_decision_on_choice(icd10_metainfo, key, exclusion_list)
-        if icd10_metainfo.get(key).hcc_map is not None and self.get_exclusion_list_hccmap(exclusion_list,
-                                                                                          icd10_metainfo) is False:
+
+        if icd10_metainfo.get(key).hcc_map is not None and \
+                self.get_exclusion_list_hccmap(exclusion_list, icd10_metainfo) is False:
             return [key]
 
-        if icd10_metainfo.get(key).hcc_map is None and self.get_exclusion_list_hccmap(exclusion_list,
-                                                                                      icd10_metainfo) is True:
+        if icd10_metainfo.get(key).hcc_map is None and \
+                self.get_exclusion_list_hccmap(exclusion_list, icd10_metainfo) is True:
             return exclusion_list
 
-        if icd10_metainfo.get(key).hcc_map is not None and self.get_exclusion_list_hccmap(exclusion_list,
-                                                                                  icd10_metainfo) is True:
+        if icd10_metainfo.get(key).hcc_map is not None and \
+                self.get_exclusion_list_hccmap(exclusion_list, icd10_metainfo) is True:
             if len(exclusion_list) > 1:
-                count = 0
-                for each_elem in exclusion_list:
-                    if icd10_metainfo.get(each_elem).hcc_map is not None:
-                        count += 1
-                if count > 1:
+                count = [1 if icd10_metainfo.get(each_elem).hcc_map is not None else 0 for each_elem in exclusion_list]
+                if sum(count) > 1:
                     return exclusion_list
             return self.get_decision_on_choice(icd10_metainfo, key, exclusion_list)
 
-        return resultant_icd10s
+        return []
