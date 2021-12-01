ae7ad93324eb74121505df036b5d6914b7913c4d
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2021-12-01 15:46:34 +0600

    add patient info to dto

--- app/dto/request/icd10_annotation_request.py
+++ app/dto/request/icd10_annotation_request.py
@@ -8,6 +8,8 @@ from app.dto.base_dto import BaseDto
 class ICD10AnnotationRequest(BaseDto):
     id: str
     text: str
+    sex: str
+    age: float
 
     @validator('id')
     def id_must_be_string_and_nonempty(cls, id_value: str):
@@ -19,4 +21,10 @@ class ICD10AnnotationRequest(BaseDto):
     def text_must_be_string_and_nonempty(cls, text: str):
         if len(text.strip()) == 0 or re.match(r"^true$|^false$|^[0-9.]+$", text.strip().lower()):
             raise ValueError("text must be string and cannot be empty")
+        return text
+
+    @validator('sex')
+    def sex_must_be_string_and_nonempty(cls, text: str):
+        if len(text.strip()) == 0 or re.match(r"^M|F$", text.strip().lower()):
+            raise ValueError("sex must be M or F")
         return text
