c1bd6aee7d7f5a7703839b2e9841527ba0f4e87b
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2021-12-22 12:41:30 +0600

    save word changes from negation fixing

--- app/service/pipeline/components/negation_processing_component.py
+++ app/service/pipeline/components/negation_processing_component.py
@@ -1,12 +1,15 @@
-from app.settings import Settings
+import re
+
+
+from app.dto.pipeline.changed_word_annotation import ChangedWordAnnotation
 from app.service.icd10_negation_service import ICD10NegationService
 from app.service.impl.icd10_negation_service_impl import Icd10NegationServiceImpl
 from app.service.pipeline.components.base_pipeline_component import BasePipelineComponent
+from app.settings import Settings
 from app.util.dependency_injector import DependencyInjector
 
 
 class NegationHandlingComponent(BasePipelineComponent):
-
     DEPENDS_ON = []
 
     def __init__(self):
@@ -15,15 +18,34 @@ class NegationHandlingComponent(BasePipelineComponent):
             Icd10NegationServiceImpl)
 
     def run(self, annotation_results: dict) -> list:
+        changed_words = {}
         if annotation_results['acm_cached_result'] is not None:
             return []
         tokenize = Settings.get_settings_tokenizer()
         text = annotation_results['text']
         tokens = tokenize(text.lower())
         text_tokens = [each.text for each in tokens]
-        for index, each_token in enumerate(text_tokens):
+        for index, token in enumerate(tokens):
+            each_token = token.text.lower()
             if each_token.lower().find("no") == 0:
-                text_tokens[index] = self.__icd10_negation_fixing_service.get_icd_10_text_negation_fixed(each_token)
+                fixed_token = self.__icd10_negation_fixing_service.get_icd_10_text_negation_fixed(each_token)
+                text_tokens[index] = fixed_token
+                self._track_text_change(fixed_token, each_token, changed_words, token)
+
         text_tokens = [" " + each_token if each_token not in [",", "?", "!", ".", ";", ":"] else each_token
                        for each_token in text_tokens]
+        annotation_results["changed_words"].update(changed_words)
         return ["".join(text_tokens).strip()]
+
+    def _track_text_change(self, fixed_token, each_token, changed_words, token):
+        fixed_words = re.sub(r"[^\w]", " ", fixed_token).split()
+        if len(fixed_words) < 2:
+            return
+        value = each_token.replace("no", "").strip()
+        key = fixed_words[1]
+        start = token.idx + each_token.find(value)
+        end = token.idx + len(each_token)
+        if key in changed_words:
+            changed_words[key].append(ChangedWordAnnotation(key, value, start, end))
+        else:
+            changed_words[key] = [ChangedWordAnnotation(key, value, start, end)]
