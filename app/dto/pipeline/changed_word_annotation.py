31986c85cc227ec2e040e93410b3d94475e205a8
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2021-12-22 12:40:41 +0600

    dto to track changed words

---
+++ app/dto/pipeline/changed_word_annotation.py
@@ -0,0 +1,7 @@
+class ChangedWordAnnotation:
+    def __init__(self, changed_text, original_text, start, end):
+        self.changed_text = changed_text
+        self.original_text = original_text
+        self.start = start
+        self.end = end
+
