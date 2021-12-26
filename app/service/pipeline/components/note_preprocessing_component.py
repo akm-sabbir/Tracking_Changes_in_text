8381bb00b632cd28d5fb5b5f99007abebb742e07
akm.sabbir <sabbirsourov@gmail.com>
2021-12-26 22:03:28 +0600

    fixed the return type for NoteProcessingComponent

--- app/service/pipeline/components/note_preprocessing_component.py
+++ app/service/pipeline/components/note_preprocessing_component.py
@@ -1,4 +1,5 @@
 from typing import List
+from app.dto.core.pipeline.paragraph import Paragraph
 from app.service.pipeline.components.base_pipeline_component import BasePipelineComponent
 from app.service.pipeline.components.negation_processing_component import NegationHandlingComponent
 from app.util.config_manager import ConfigManager
@@ -9,7 +10,7 @@ from app.dto.pipeline.negation_component_result import NegationResult
 class NotePreprocessingComponent(BasePipelineComponent):
     DEPENDS_ON = [NegationHandlingComponent]
 
-    def run(self, annotation_results: dict) -> List[BasePipelineComponent]:
+    def run(self, annotation_results: dict) -> List[Paragraph]:
         if annotation_results['acm_cached_result'] is not None:
             return []
         text: NegationResult = annotation_results[NegationHandlingComponent][0]
