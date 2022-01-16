01766a15ad6eef1563c34a35b9828ee33a0d030c
akm.sabbir <sabbirsourov@gmail.com>
2022-01-16 18:47:20 +0600

    changed the smoker detector context to understand patients query

--- tests/service/impl/test_icd10_smoking_pattern_decision_impl.py
+++ tests/service/impl/test_icd10_smoking_pattern_decision_impl.py
@@ -14,4 +14,4 @@ class TestICD10SmokingPatternDecisionImpl(TestCase):
         text = "as the patient queried about smoking behavior? Yes No\n" \
                "Does the patient currently smoke? Smoking: Patient has never smoked - (1/21/2020).\n"
         results = self.icd10SmokingPatternDetect.get_smoking_pattern_decision(text)
-        assert results is True
+        assert results is False
