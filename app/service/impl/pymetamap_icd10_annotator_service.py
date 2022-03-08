298638c1abe7d6178ad34aec81380168f504c827
fuadrafid <muhtasim.fuad@infolytx.com>
2022-03-08 12:04:03 +0600

    update metamap service to call external url

--- app/service/impl/pymetamap_icd10_annotator_service.py
+++ app/service/impl/pymetamap_icd10_annotator_service.py
@@ -1,42 +1,43 @@
-import os
 import re
 from typing import List, Dict
-
-from pymetamap import MetaMap, ConceptMMI, ConceptAA
 
 from app.dto.pipeline.icd10_annotation import ICD10Annotation
 from app.dto.pipeline.icd10_annotation_result import ICD10AnnotationResult
 from app.service.icd10_annotator_service import ICD10AnnotatorService
 from app.service.impl.cui_to_icd10_service_impl import CUItoICD10ServiceImpl
+from app.util.config_manager import ConfigManager
 from app.util.dependency_injector import DependencyInjector
+from app.util.rest_client_util import RestClientUtil
+
+
+class MetamapRequest:
+    def __init__(self, text: str):
+        self.text: str = text
 
 
 class PymetamapICD10AnnotatorService(ICD10AnnotatorService):
 
     def __init__(self):
-        self.mapper_service = MetaMap.get_instance(os.getenv('METAMAP_PATH'))
         self.icd10_mapper_service: CUItoICD10ServiceImpl = DependencyInjector.get_instance(CUItoICD10ServiceImpl)
 
     def get_icd_10_codes(self, text: str) -> List:
-        concepts, error = self.mapper_service.extract_concepts([text.replace("\n", " ")],
-                                                               restrict_to_sources=["ICD10CM"])
-        return self._map_to_annotation_result_dto(text, concepts)
+        response = RestClientUtil.post_sync(ConfigManager.get_specific_config("metamap", "service_url"), {'text': text})
+        return self._map_to_annotation_result_dto(text, response.json())
 
-    def _map_to_annotation_result_dto(self, text: str, concepts: List[ConceptMMI]) -> List[ICD10AnnotationResult]:
+    def _map_to_annotation_result_dto(self, text: str, concepts) -> List[ICD10AnnotationResult]:
         unique_concepts: Dict[str, ICD10AnnotationResult] = {}
         for concept in concepts:
-
-            if not isinstance(concept, ConceptMMI):
+            if "mm" not in concept:
                 continue
-            icd10_data = self.icd10_mapper_service.get_umls_data_from_cui(concept.cui)
+            icd10_data = self.icd10_mapper_service.get_umls_data_from_cui(concept["cui"])
             if icd10_data.icd10 == "":
                 continue
             icd10_annotations = [ICD10Annotation(code=icd10_data.icd10,
                                                  description=icd10_data.concept,
-                                                 score=concept.score)]
+                                                 score=concept["score"])]
 
             # position info is separated by ; . We take the first position info only
-            position_string = concept.pos_info.split(";")[0]
+            position_string = concept["posInfo"].split(";")[0]
             if position_string in unique_concepts:
                 unique_concepts[position_string].suggested_codes.extend(icd10_annotations)
                 continue
--- tests/service/impl/test_pymetamap_icd10_annotation_service.py
+++ tests/service/impl/test_pymetamap_icd10_annotation_service.py
@@ -2,8 +2,6 @@ from typing import List
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
-from pymetamap import ConceptMMI
-
 from app.dto.core.umls_icd10_data import UMLSICD10Data
 from app.dto.pipeline.icd10_annotation_result import ICD10AnnotationResult
 from app.service.impl.pymetamap_icd10_annotator_service import PymetamapICD10AnnotatorService
@@ -12,45 +10,33 @@ from app.service.impl.pymetamap_icd10_annotator_service import PymetamapICD10Ann
 class TestPymetamapICD10AnnotationService(TestCase):
 
     @patch("app.service.impl.pymetamap_icd10_annotator_service.DependencyInjector.get_instance")
-    @patch("app.service.impl.pymetamap_icd10_annotator_service.MetaMap.get_instance")
-    def test__get_icd_10_codes__should_return_correct_values__given_correct_input(self, mock_metamap: Mock,
+    @patch("app.util.config_manager.ConfigManager.get_specific_config")
+    @patch("app.util.rest_client_util.RestClientUtil.post_sync")
+    def test__get_icd_10_codes__should_return_correct_values__given_correct_input(self, mock_post: Mock,
+                                                                                  mock_config_manager: Mock,
                                                                                   mock_injector: Mock):
-        metamap_mock = Mock()
+        mock_config_manager.return_value = "some url"
         icd10_mapper_mock = Mock()
 
-        metamap_mock.extract_concepts = Mock()
-        concept1 = Mock(ConceptMMI)
-        concept1.cui = "123"
-        concept1.pos_info = "1/4;2/6"
-        concept1.score = 5.13
+        concept1 = {"cui": "123", "mm": "MMI", "posInfo": "1/4;2/6", "score": "5.13"}
+        concept2 = {"cui": "127", "mm": "MMI", "posInfo": "1/4", "score": "5.13"}
+        concept3 = {"cui": "123", "mm": "MMI", "posInfo": "[6/1,8/2], [1/6,3/6]", "score": "5.13"}
+        concept4 = {"cui": "123", "mm": "MMI", "posInfo": "1/2,2/3", "score": "5.13"}
 
-        concept2 = Mock(ConceptMMI)
-        concept2.cui = "127"
-        concept2.pos_info = "1/4"
-        concept2.score = 5.13
+        mock_response = Mock()
+        mock_response.json = Mock()
+        mock_response.json.return_value = [concept1, concept2, concept3, concept4]
 
-        concept3 = Mock(ConceptMMI)
-        concept3.cui = "123"
-        concept3.pos_info = "[6/1,8/2], [1/6,3/6]"
-        concept3.score = 5.13
-
-        concept4 = Mock(ConceptMMI)
-        concept4.cui = "123"
-        concept4.pos_info = "1/2,2/3"
-        concept4.score = 5.13
-
-        metamap_mock.extract_concepts.return_value = ([concept1, concept2, concept3, concept4], None)
+        mock_post.return_value = mock_response
 
         icd10_mapper_mock.get_umls_data_from_cui = Mock()
         icd10_mapper_mock.get_umls_data_from_cui.return_value = UMLSICD10Data("123", "concept", "definition",
                                                                               "R123.123")
 
-        mock_metamap.return_value = metamap_mock
         mock_injector.return_value = icd10_mapper_mock
 
         metamap_service = PymetamapICD10AnnotatorService()
 
-        metamap_mock.extract_concepts()
         result: List[ICD10AnnotationResult] = metamap_service.get_icd_10_codes("some text")
 
         assert result[0].medical_condition == "some"
@@ -81,29 +67,31 @@ class TestPymetamapICD10AnnotationService(TestCase):
         assert result[2].suggested_codes[0].score == 5.13
 
     @patch("app.service.impl.pymetamap_icd10_annotator_service.DependencyInjector.get_instance")
-    @patch("app.service.impl.pymetamap_icd10_annotator_service.MetaMap.get_instance")
-    def test__get_icd_10_codes__should_return_empty__given_no_icd110_mappings(self, mock_metamap: Mock,
-                                                                              mock_injector: Mock):
-        metamap_mock = Mock()
+    @patch("app.util.config_manager.ConfigManager.get_specific_config")
+    @patch("app.util.rest_client_util.RestClientUtil.post_sync")
+    def test__get_icd_10_codes__should_return_empty__given_no_icd10_mappings(self, mock_post: Mock,
+                                                                             mock_config_manager: Mock,
+                                                                             mock_injector: Mock):
+        mock_config_manager.return_value = "some url"
         icd10_mapper_mock = Mock()
 
-        metamap_mock.extract_concepts = Mock()
-        concept = Mock(ConceptMMI)
-        concept.cui = "123"
-        concept.pos_info = "1/4"
-        concept.score = 5.13
-        metamap_mock.extract_concepts.return_value = ([concept], None)
+        concept1 = {"cui": "123", "mm": "MMI", "posInfo": "1/4;2/6", "score": "5.13"}
+        concept2 = {"cui": "127", "mm": "MMI", "posInfo": "1/4", "score": "5.13"}
+        concept3 = {"cui": "123", "mm": "MMI", "posInfo": "[6/1,8/2], [1/6,3/6]", "score": "5.13"}
+        concept4 = {"cui": "123", "mm": "MMI", "posInfo": "1/2,2/3", "score": "5.13"}
+
+        mock_response = Mock()
+        mock_response.json = Mock()
+        mock_response.json.return_value = [concept1, concept2, concept3, concept4]
+
+        mock_post.return_value = mock_response
 
         icd10_mapper_mock.get_umls_data_from_cui = Mock()
-        icd10_mapper_mock.get_umls_data_from_cui.return_value = UMLSICD10Data("123", "concept", "definition",
-                                                                              "")
+        icd10_mapper_mock.get_umls_data_from_cui.return_value = UMLSICD10Data("123", "concept", "definition", "")
 
-        mock_metamap.return_value = metamap_mock
         mock_injector.return_value = icd10_mapper_mock
 
         metamap_service = PymetamapICD10AnnotatorService()
-
-        metamap_mock.extract_concepts()
         result: List[ICD10AnnotationResult] = metamap_service.get_icd_10_codes("some text")
 
         assert len(result) == 0
