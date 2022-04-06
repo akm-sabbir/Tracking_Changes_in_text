4355a5ef6978773e1f1109eb8016de65452b6b21
Muhtasim Fuad <muhtasim.fuad@infolytx.com>
2022-04-06 08:17:33 +0000

    Revert "PA-1033 implement algorithm for exclusion list (pull request #53)"

--- app/dto/core/service/exclusion_graph.py
+++
@@ -1,5 +0,0 @@
-class ExclusionGraph:
-    code: str
-    vote: int = 0
-    neighbors: dict = dict()
-    degree: int = 0
--- app/service/icd10_exclusion_service.py
+++ app/service/icd10_exclusion_service.py
@@ -5,5 +5,5 @@ from typing import List
 
 class ICD10ExclusionService(ABC):
     @abstractmethod
-    def get_icd10_code_exclusion_decision_based_graph(self, icd_data_dict: dict) -> str:
+    def get_icd_10_code_exclusion_decision(self, icd_data_dict: dict) -> str:
         raise NotImplementedError()
