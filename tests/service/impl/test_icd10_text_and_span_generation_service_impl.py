e388a2fc7c46301dbc2f459ad2fc49e6abe26e31
root <you@example.com>
2022-04-13 12:35:24 +0600

    added more unit test cases

--- tests/service/impl/test_icd10_text_and_span_generation_service_impl.py
+++ tests/service/impl/test_icd10_text_and_span_generation_service_impl.py
@@ -12,6 +12,13 @@ class TestICD10TextAndSpanGenerationServiceImplTest(TestCase):
                "Does the patient currently smoke? Smoking: Patient has never smoked - (1/21/2020).\n"
         test_result1 = self.icd10TextTokenGenerator.get_token_with_span(test_text1)
         actual_test_result1 = self.icd10TextTokenGenerator.process_each_token(test_result1)
+        print(actual_test_result1)
+        count = sum([1 if actual_test_result1[i][0] == '?' else 0 for i in range(len(actual_test_result1))])
+        count_paren = sum([1 if actual_test_result1[i][0] == '(' else 0 for i in range(len(actual_test_result1))])
+        count_colon = sum([1 if actual_test_result1[i][0] == '(' else 0 for i in range(len(actual_test_result1))])
+        assert count == 2
+        assert count_paren == 1
+        assert count_colon == 1
         assert actual_test_result1[2][1] == 7
         assert actual_test_result1[2][2] == 14
         assert actual_test_result1[6][0] == "behavior"
@@ -26,10 +33,11 @@ class TestICD10TextAndSpanGenerationServiceImplTest(TestCase):
            "he also chronic diarrheafrom time to time,  the absence of recurrent leg cramps is obvious"
         test_result1 = self.icd10TextTokenGenerator.get_token_with_span(test_text1)
         actual_test_result1 = self.icd10TextTokenGenerator.process_each_token(test_result1)
-        print(actual_test_result1)
+        count = sum([1 if actual_test_result1[i][0] == ',' else 0 for i in range(len(actual_test_result1))])
         assert actual_test_result1[2][1] == 7
         assert actual_test_result1[2][2] == 11
         assert actual_test_result1[7][0] == "continues"
         assert  actual_test_result1[11][0] == "daily"
         assert actual_test_result1[21][0] == 'fall'
-        assert  actual_test_result1[50][0] == 'incontinent'
+        assert actual_test_result1[50][0] == 'incontinent'
+        assert count == 14
