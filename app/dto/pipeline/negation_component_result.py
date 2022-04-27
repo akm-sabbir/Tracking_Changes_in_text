2b9bbb18505e0f237f9f019afafd77a93bb7aefc
AKM Sabbir <akm.sabbir@infolytx.com>
2022-04-27 11:52:51 +0600

    added required changes to code

--- app/dto/pipeline/negation_component_result.py
+++ app/dto/pipeline/negation_component_result.py
@@ -1,6 +1,8 @@
 from app.dto.pipeline.base_pipeline_component_result import BasePipelineComponentResult
+from app.dto.core.service.Tokens import TokenInfo
+from typing import List
 
 
 class NegationResult(BasePipelineComponentResult):
-    def __init__(self, text: str):
-        self.text: str = text
+    def __init__(self, token_info_with_span: List[TokenInfo]):
+        self.tokens_with_span: str = token_info_with_span
--- app/service/pipeline/components/negation_processing_component.py
+++ app/service/pipeline/components/negation_processing_component.py
@@ -1,14 +1,9 @@
-import re
 from typing import Dict, List
-
-import spacy
-from spacy.tokens import Token
 
 from app.dto.core.service.Tokens import TokenInfo
 from app.service.icd10_negation_service import ICD10NegationService
 from app.service.impl.icd10_negation_service_impl import Icd10NegationServiceImpl
 from app.service.pipeline.components.base_pipeline_component import BasePipelineComponent
-from app.service.pipeline.components.icd10_tokenizing_text_component import TextTokenizationComponent
 from app.service.pipeline.components.medication_section_extractor_component import MedicationSectionExtractorComponent
 from app.service.pipeline.components.subjective_section_extractor_component import SubjectiveSectionExtractorComponent
 from app.service.pipeline.components.icd10_tokenizing_text_component import TextTokenizationComponent
@@ -17,6 +12,7 @@ from app.util.dependency_injector import DependencyInjector
 from app.util.pipeline_util import PipelineUtil
 from app.dto.pipeline.negation_component_result import NegationResult
 from app.util.text_span_discovery import TextSpanDiscovery
+from app.service.pipeline.components.icd10_token_to_graph_generation_component import GraphTokenResult
 
 
 class NegationHandlingComponent(BasePipelineComponent):
@@ -33,25 +29,23 @@ class NegationHandlingComponent(BasePipelineComponent):
             return []
         tokenizer = Settings.get_settings_tokenizer()
 
-        subjective_section_text_tokens = self.__fix_negation_for_section(
+        updated_graph, new_subjective_section_text_tokens = self.__fix_negation_for_section(
                                                                          annotation_results[
                                                                             TextTokenizationComponent][
                                                                             0].token_container,
-                                                                         annotation_results)
-
-
-        medication_section_text_tokens = self.__fix_negation_for_section(
+                                                                         annotation_results[GraphTokenResult][0])
+        annotation_results[GraphTokenResult][0] = updated_graph
+        updated_graph, new_medication_section_text_tokens = self.__fix_negation_for_section(
                                                                          annotation_results[
                                                                             TextTokenizationComponent][
                                                                             1].token_container,
-                                                                         annotation_results)
-        subjective_section_text = "".join(subjective_section_text_tokens).strip()
-        medication_section_text = "".join(medication_section_text_tokens).strip()
+                                                                         annotation_results[GraphTokenResult][1])
+        annotation_results[GraphTokenResult][1] = updated_graph
 
-        return [NegationResult(text=subjective_section_text),
-                NegationResult(text=medication_section_text)]
+        return [NegationResult(token_info_with_span=new_subjective_section_text_tokens),
+                NegationResult(token_info_with_span=new_medication_section_text_tokens)]
 
-    def __fix_negation_for_section(self, token_container: List[TokenInfo], annotation_results: dict):
+    def __fix_negation_for_section(self, token_container: List[TokenInfo], token_graphs: dict):
         changed_token_dict = {}
         for index, token in enumerate(token_container):
             each_token = token.token.lower()
@@ -59,11 +53,11 @@ class NegationHandlingComponent(BasePipelineComponent):
             if each_token.find("no") == 0:
                 fixed_token = self.__icd10_negation_fixing_service.get_icd_10_text_negation_fixed(each_token)
                 changed_token_dict[each_token] = fixed_token
-        self._track_text_change_part(changed_token_dict, token_container)
+        updated_graphs, new_text_span = self._track_text_change_part(changed_token_dict, token_container, token_graphs)
 
-        return (" " + each_token if each_token not in [",", "?", "!", ".", ";", ":"] else each_token
-                for each_token in text_tokens)
+        return
 
-    def _track_text_change_part(self,dictionary: dict, token_container):
+    def _track_text_change_part(self,dictionary: dict, token_container: List[TokenInfo], token_graphs):
         self.__text_span_discovery.set_changed_text_dictionary(dictionary)
-        self.__text_span_discovery.generate_metainfo_for_changed_text()
+        updated_graphs, new_text_span = self.__text_span_discovery.generate_metainfo_for_changed_text(token_graphs, token_container)
+        return updated_graphs, new_text_span
