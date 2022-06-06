f1f280a67281b75e648fb73209b7235b3ed2a0de
muhtasimfuadrafid <muhtasim.fuad@infolytx.com>
2022-06-06 12:26:59 +0600

    update tests

--- tests/service/impl/test_scispacy_icd10_annotator_service.py
+++ tests/service/impl/test_scispacy_icd10_annotator_service.py
@@ -49,6 +49,40 @@ class TestScispacyICD10AnnotatorService(TestCase):
             ICD10Annotation(code='R123.123', description='concept', score=0.67),
             ICD10Annotation(code='R123.123', description='concept', score=0.97)]
 
+    @patch('app.service.impl.scispacy_icd10_annotator_service.ConfigManager.get_specific_config', Mock())
+    @patch('app.service.impl.scispacy_icd10_annotator_service.termset', Mock())
+    @patch('app.service.impl.scispacy_icd10_annotator_service.spacy.load')
+    @patch('app.service.impl.scispacy_icd10_annotator_service.DependencyInjector.get_instance')
+    def test__get_icd_10_codes__should_return_empty_response__given_incorrect_input(self,
+                                                                                    mock_icd10_mapper: Mock,
+                                                                                    mock_nlp: Mock):
+        icd10_mapper_mock = Mock()
+        icd10_mapper_mock.get_icd10_from_cui = Mock()
+        icd10_mapper_mock.get_umls_data_from_cui = Mock()
+        icd10_mapper_mock.get_umls_data_from_cui.return_value = UMLSICD10Data("", None, None,
+                                                                              "")
+
+        icd10_mapper_mock.get_icd10_from_cui.return_value = ""
+
+        mock_icd10_mapper.return_value = icd10_mapper_mock
+        mock_nlp.return_value = self.__get_dummy_spacy_object()
+
+        scispacy_annotator: ICD10AnnotatorService = ScispacyICD10AnnotatorService()
+
+        mock_icd10_codes = scispacy_annotator.get_icd_10_codes("Diabetes, heart attack.")
+
+        mock_nlp.assert_called_once()
+
+        assert mock_icd10_codes[0].medical_condition == "some entity 1"
+        assert mock_icd10_codes[0].begin_offset == 0
+        assert mock_icd10_codes[0].end_offset == 12
+        assert mock_icd10_codes[0].suggested_codes == []
+
+        assert mock_icd10_codes[1].medical_condition == "some entity 2"
+        assert mock_icd10_codes[1].begin_offset == 14
+        assert mock_icd10_codes[1].end_offset == 25
+        assert mock_icd10_codes[1].suggested_codes == []
+
     def __get_dummy_spacy_object(self):
         mock_entities = Mock(Doc)
         mock_entities.ents = self.__get_dummy_entities()
