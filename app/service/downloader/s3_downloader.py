8173c203a954bd0441ea7f68841af8596236d3ff
Arefeen <arefeen.sultan@infolytx.com>
2022-07-24 19:35:16 +0600

    S3 downloader updated.

--- app/service/downloader/s3_downloader.py
+++ app/service/downloader/s3_downloader.py
@@ -7,8 +7,6 @@ from app.exception.exception_messages import S3_DOWNLOAD_ERROR
 
 
 class S3DownloaderService:
-    __logger = logging.getLogger(__name__)
-
     def __init__(self):
         self.s3 = boto3.resource('s3')
 
@@ -16,11 +14,5 @@ class S3DownloaderService:
         try:
             if not os.path.exists(local_file_name):
                 self.s3.meta.client.download_file(bucket, key, local_file_name)
-            else:
-                self.__logger.error(f'{bucket}/{key} is already downloaded by someone else')
-        except Exception as e:
-            error_message = "Error occurred during downloading file: " + key + " from bucket " + bucket + \
-                            " to -> " + local_file_name + " Reason: " + str(e)
-            self.__logger.error(error_message)
-
+        except Exception:
             raise RuntimeError(S3_DOWNLOAD_ERROR)
